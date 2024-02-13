180d705db32e7cc2d9a1270f69b9578a34f1d089
Brett Bendickson <bbendick@arizona.edu>
2024-02-13 14:46:06 -0700

    Changed ID of prod molecule

--- boomi_scheduled_jobs.py
+++ boomi_scheduled_jobs.py
@@ -27,7 +27,7 @@ def getJobs(atomId, label):
     return
 
 st.sidebar.title('Boomi - Scheduled Jobs')
-st.sidebar.button('prod-trellis-molecule', on_click=getJobs, type="primary", args=['eea33c78-01ad-4ebb-a511-b9c8bd0ea16a', 'Prod Molecule'])
+st.sidebar.button('prod-trellis-molecule', on_click=getJobs, type="primary", args=['3d78acc2-9f2b-41ff-bbfd-a3f2ed30c89e', 'Prod Molecule'])
 st.sidebar.button('prod-trellis-atom', on_click=getJobs, type="primary", args=['cdcca9c9-0797-4934-9b83-6e127385ef7f', 'Prod Atom'])
 st.sidebar.button('nonprod-qa-molecule', on_click=getJobs, type="primary", args=['76a40e65-b51a-4378-a48f-8ff4f7a90674', 'QA Molecule'])
 st.sidebar.button('nonprod-qa-atom', on_click=getJobs, type="primary", args=['81b83d93-cdcc-4801-ad79-d3557295b960', 'QA Atom'])
